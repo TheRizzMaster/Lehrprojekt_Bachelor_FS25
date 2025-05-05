60b9890a7b476abeb7f6adfd81f7a895d5b5e312
siro rudolf <siro.rudolf@stud.fhgr.ch>
2025-05-05 14:13:31 +0200

    update feedback

--- js/plattform_feedback.js
+++ js/plattform_feedback.js
@@ -32,18 +32,20 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    const sliders = document.querySelectorAll('input[type="range"]');
 
-    sliders.forEach(slider => {
-    function updateBackground(el) {
-        const val = (el.value - el.min) / (el.max - el.min) * 100;
-        el.style.background = `linear-gradient(to right, #3182ce 0%, #3182ce ${val}%, #e2e8f0 ${val}%, #e2e8f0 100%)`;
-    }
-    
-    updateBackground(slider);
-    
-    slider.addEventListener('input', () => {
-        updateBackground(slider);
-    });
-    });
+  });
+
+  const sliders = document.querySelectorAll('input[type="range"]');
+
+  sliders.forEach(slider => {
+  function updateBackground(el) {
+      const val = (el.value - el.min) / (el.max - el.min) * 100;
+      el.style.background = `linear-gradient(to right, #3182ce 0%, #3182ce ${val}%, #e2e8f0 ${val}%, #e2e8f0 100%)`;
+  }
+  
+  updateBackground(slider);
+  
+  slider.addEventListener('input', () => {
+      updateBackground(slider);
+  });
   });
